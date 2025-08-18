8800aef4c5db85437087a2b9bc412cb42e0d9757
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-08-19 01:15:59 +0300

    feat: added back button

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,8 @@
+import { Button } from "@/components/ui/button";
 import { Post, getPostBySlug, getAllPosts } from "@/lib/posts"
+import { ChevronLeft } from "lucide-react";
+import Link from "next/link"
+
 
 interface PostPageProps {
     params: Promise<{ slug: string }>;
@@ -14,6 +18,17 @@ export default async function PostPage({ params }: PostPageProps) {
     const post: Post = await getPostBySlug(slug);
 
   return (
+    <>
+    <Link
+        href="/blog"
+    >
+        <Button
+            variant={"secondary"}
+        >
+            <ChevronLeft/>
+            Πίσω
+        </Button>
+    </Link>
     <main className="container mx-auto px-4 py-10">
       {/* Title + Date */}
       <h1 className="text-4xl font-bold mb-2">{post.title}</h1>
@@ -24,5 +39,6 @@ export default async function PostPage({ params }: PostPageProps) {
         <div dangerouslySetInnerHTML={{ __html: post.contentHtml }} />
       </article>
     </main>
+    </>
   )
 }
