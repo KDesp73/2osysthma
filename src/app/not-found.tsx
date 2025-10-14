3b6d2ca19843e9e5d666789b6b2fc0d176720e30
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-10-14 22:53:15 +0300

    feat: improved 404 page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center">
       <h1 className="text-6xl font-bold">404</h1>
-      <p className="text-xl mt-4">Sorry, we couldn’t find that page.</p>
+      <p className="text-xl mt-4 mb-4">Δεν μπορούσαμε να βρούμε την σελίδα που ψάχνεις. Μάλλον είναι μαζί με την μπεπανθόλ</p>
+      <Image
+        src={"/bepanthol.jpg"}
+        alt="bepanthol"
+        width={800}
+        height={600}
+      >
+      </Image>
       <Link href="/" className="mt-6 text-blue-600 underline">
-        Go back home
+        Επέστρεψε στην Αρχική
       </Link>
     </div>
   );
