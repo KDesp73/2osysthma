c8dcb86c68707d7a647cf19e37df5b908ceeac33
Konstantinos Despoinidis <kdesp2003@gmail.com>
2025-12-06 11:28:13 +0200

    fix: build

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.143", "192.168.1.8"],
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
 };
 
 export default nextConfig;
